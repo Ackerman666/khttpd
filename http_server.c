b66ddb405bb7528ec9f6f5cab5f9d9abd460f02e
楊景翔 <zzz6992926gmail.com>
2024-06-20 06:23:01 +0800

    Implement basic directory listing functionality
    Add "directory_listing" to trace directory item and show them

--- http_server.c
+++ http_server.c
@@ -1,5 +1,8 @@
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
+#include <linux/dcache.h>
+#include <linux/fs.h>
+#include <linux/fs_struct.h>
 #include <linux/kthread.h>
 #include <linux/sched/signal.h>
 #include <linux/tcp.h>
@@ -20,6 +23,14 @@
     "HTTP/1.1 200 OK" CRLF "Server: " KBUILD_MODNAME CRLF     \
     "Content-Type: text/plain" CRLF "Content-Length: 12" CRLF \
     "Connection: Keep-Alive" CRLF CRLF "Hello World!" CRLF
+
+#define HTTP_RESPONSE_200_KEEPALIVE_HTML                  \
+    ""                                                    \
+    "HTTP/1.1 200 OK" CRLF "Server: " KBUILD_MODNAME CRLF \
+    "Connection: Keep-Alive" CRLF "Hello World!" CRLF     \
+    "Keep-Alive: timeout=5" CRLF "Content-Type: text/html" CRLF CRLF
+
+
 #define HTTP_RESPONSE_501                                              \
     ""                                                                 \
     "HTTP/1.1 501 Not Implemented" CRLF "Server: " KBUILD_MODNAME CRLF \
@@ -32,6 +43,15 @@
     "Connection: KeepAlive" CRLF CRLF "501 Not Implemented" CRLF
 
 #define RECV_BUFFER_SIZE 4096
+#define SEND_BUFFER_SIZE 4096
+
+
+struct dir_tracer {
+    struct dir_context dir_context;
+    struct socket *socket;
+};
+
+
 
 struct request_handler {
     struct work_struct w;
@@ -44,6 +64,7 @@ struct http_request {
     char request_url[128];
     int complete;
 };
+
 
 static int http_server_recv(struct socket *sock, char *buf, size_t size)
 {
@@ -79,17 +100,77 @@ static int http_server_send(struct socket *sock, const char *buf, size_t size)
     return done;
 }
 
+static bool trace_directory(struct dir_context *dir_context,
+                            const char *name,
+                            int name_size,
+                            loff_t offset,
+                            u64 ino,
+                            unsigned int d_type)
+{
+    //"." represents the current directory, ".." represents the parent
+    // directory, and neither of them will be displayed.
+    if (strcmp(name, ".") && strcmp(name, "..")) {
+        struct dir_tracer *tracer =
+            container_of(dir_context, struct dir_tracer, dir_context);
+        char buf[SEND_BUFFER_SIZE] = {0};
+
+        snprintf(buf, SEND_BUFFER_SIZE,
+                 "<tr><td><a href=\"%s\">%s</a></td></tr>\r\n", name, name);
+        http_server_send(tracer->socket, buf, strlen(buf));
+    }
+
+    return true;
+}
+
+
+
+static void directory_listing(struct http_request *request)
+{
+    struct dir_tracer d_tracer = {
+        .dir_context =
+            {
+                .actor = trace_directory,
+            },
+        .socket = request->socket,
+    };
+
+    struct file *fp;
+    char buf[SEND_BUFFER_SIZE] = {0};
+
+    fp = filp_open("/home/xiang/Desktop/linux2024/khttpd", O_RDONLY, 0);
+
+    if (IS_ERR(fp)) {
+        pr_info("Open file failed");
+        return;
+    }
+
+    snprintf(buf, SEND_BUFFER_SIZE, HTTP_RESPONSE_200_KEEPALIVE_HTML);
+    http_server_send(request->socket, buf, strlen(buf));
+    snprintf(buf, SEND_BUFFER_SIZE, "%s%s%s%s", "<html><head><style>\r\n",
+             "body{font-family: monospace; font-size: 15px;}\r\n",
+             "td {padding: 1.5px 6px;}\r\n",
+             "</style></head><body><table>\r\n");
+    http_server_send(request->socket, buf, strlen(buf));
+
+    iterate_dir(fp, &d_tracer.dir_context);
+    snprintf(buf, SEND_BUFFER_SIZE, "</table></body></html>\r\n");
+    http_server_send(request->socket, buf, strlen(buf));
+    filp_close(fp, NULL);
+
+    return;
+}
+
 static int http_server_response(struct http_request *request, int keep_alive)
 {
-    char *response;
-
     pr_info("requested_url = %s\n", request->request_url);
-    if (request->method != HTTP_GET)
-        response = keep_alive ? HTTP_RESPONSE_501_KEEPALIVE : HTTP_RESPONSE_501;
+    if (request->method != HTTP_GET) {
+        char *response =
+            keep_alive ? HTTP_RESPONSE_501_KEEPALIVE : HTTP_RESPONSE_501;
+        http_server_send(request->socket, response, strlen(response));
+    }
+    // response current directory list
     else
-        response = keep_alive ? HTTP_RESPONSE_200_KEEPALIVE_DUMMY
-                              : HTTP_RESPONSE_200_DUMMY;
-    http_server_send(request->socket, response, strlen(response));
+        directory_listing(request);
     return 0;
 }
 
@@ -146,6 +227,8 @@ static int http_parser_callback_message_complete(http_parser *parser)
     request->complete = 1;
     return 0;
 }
+
+
 
 /* kthread version
 static int http_server_worker(void *arg)
@@ -227,7 +310,9 @@ static void http_server_worker(struct work_struct *w)
     request.socket = socket;
     http_parser_init(&parser, HTTP_REQUEST);
     parser.data = &request;
+    // can add a thread or worker to count down, if out of time execute shutdown
     while (1) {
+        // reset timer
         int ret = http_server_recv(socket, buf, RECV_BUFFER_SIZE - 1);
         if (ret <= 0) {
             if (ret)
